5eed11b9f4649464cd62353ec4eca296fa145bdc
LD Team <desenvolvimento@lotusdesenvolvimento.com>
2021-01-05 20:17:05 -0300

    first commit

---
+++ project/lotusdelivery/src/main/java/com/lotus/lotusdelivery/LotusdeliveryApplication.java
@@ -0,0 +1,13 @@
+package com.lotus.lotusdelivery;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LotusdeliveryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LotusdeliveryApplication.class, args);
+	}
+
+}
---
+++ project/lotusdelivery/src/main/java/com/lotus/lotusdelivery/entites/OrderStatus.java
@@ -0,0 +1,7 @@
+package com.lotus.lotusdelivery.entites;
+
+public enum OrderStatus {
+	
+	PENDING, DELIVERED
+
+}
